f0bf28d0bb151b1c1134ba1ac2e0af9d034a07b0
almiranteFuchs <brunofuchs2002@gmail.com>
2023-01-25 15:12:40 -0300

    feat: scrapper done

--- src/scrapper/scrapper.ts
+++ src/scrapper/scrapper.ts
@@ -1,15 +1,19 @@
 import { Refeicao, TipoRefeicao } from "../model/refeicao";
 import axios from "axios";
 import cherio from "cheerio";
+import { Alimento } from "../model/alimento";
+import { Cardapio } from "../model/cardapio";
 
 // I suck balls at scraping. if you see this, implement interfaces for object building and work on this scraping.
 export class Scrapper {
-    public static async get(restaurant: RestaurantLink): Promise<Refeicao> {
+    public static async get(restaurant: RestaurantLink): Promise<Cardapio[]> {
+        let cardapios: Cardapio[] = [];
+
 
         const url = restaurant;
         const axiosInstance = axios.create();
 
-        axiosInstance.get(url).then((response) => {
+        await axiosInstance.get(url).then((response: any) => {
             const html = response.data;
             const $ = cherio.load(html);
 
@@ -18,12 +22,82 @@ export class Scrapper {
             // Get the second table of the content
             const tomorrow_table = $("table").eq(1);
 
-            console.log(today_table.text());
-            
+            let today_menu = this._parseTable(today_table, $);
+            let tomorrow_menu = this._parseTable(tomorrow_table, $);
+
+
+            if (today_menu.length > 0) {
+                const cardapio_hoje = new Cardapio("", new Date(), today_menu[0], today_menu[1], today_menu[2]);
+                cardapios.push(cardapio_hoje);
+            }
+
+            if (tomorrow_menu.length > 0) {
+                const cardapio_amanha = new Cardapio("", new Date(), tomorrow_menu[0], tomorrow_menu[1], tomorrow_menu[2]);
+                cardapios.push(cardapio_amanha);
+            }
 
         });
 
-        return new Refeicao(TipoRefeicao.Almoço, []);
+        return cardapios;
+    }
+
+    private static _parseTable(table: cheerio.Cheerio, $: cheerio.Root): Refeicao[] {
+        let refeicoes: Refeicao[] = [];
+
+        // Get the second tr of the table, it's the coffe one
+        const tr = table.find("tr").eq(1);
+        let coffe = this._parseElement(tr, $);
+
+        if (coffe.length > 0) {
+            const refeicao_cafe = new Refeicao(TipoRefeicao["Cafe da manhã"], coffe);
+            refeicoes.push(refeicao_cafe);
+        }
+
+        // Get the fourth tr of the table, it's the lunch one
+        const tr2 = table.find("tr").eq(3);
+        let almoco = this._parseElement(tr2, $);
+
+        if (almoco.length > 0) {
+            const refeicao_almoco = new Refeicao(TipoRefeicao.Almoço, almoco);
+            refeicoes.push(refeicao_almoco);
+        }
+
+        // Get the sixth tr of the table, it's the dinner one
+        const tr3 = table.find("tr").eq(5);
+        let jantar = this._parseElement(tr3, $);
+
+        if (jantar.length > 0) {
+            const refeicao_jantar = new Refeicao(TipoRefeicao.Jantar, jantar);
+            refeicoes.push(refeicao_jantar);
+        }
+
+        return refeicoes;
+    }
+
+    // Specific reusable, receives "cafe tr" or "almoco tr etc"
+    private static _parseElement(element: cheerio.Cheerio, $: cheerio.Root): Alimento[] {
+        let alimentos: Alimento[] = [];
+
+        // foreach element on the td
+        element.each((index, elements) => {
+            // get the text of the element
+            //const text = $(elements).text();
+
+            // Split tag in br
+            const text = $(elements).html();
+            const text_array = text?.split("<br>");
+
+            text_array?.forEach(el => {
+                // if the text is not empty
+                if (text != "") {
+                    // Create a new Alimento
+                    const alimento = new Alimento($(el).text().trim(), [], false);     // TODO: Add the allergens and if it is vegetarian
+                    alimentos.push(alimento);
+                }
+            });
+        });
+
+        return alimentos;
     }
 
 }
